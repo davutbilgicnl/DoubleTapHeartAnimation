a26f4cec3e66e0b5d99a16fbc20e5aca14f33719
davutbilgicnl <davutbilgic.nl@gmail.com>
2024-10-23 11:24:45 +0200

    Add DoubleTabHeartAnimation component

---
+++ components/DoubleTabHeartAnimation.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const DoubleTabHeartAnimation = () => {
+  return (
+    <View>
+      <Text>DoubleTabHeartAnimation</Text>
+    </View>
+  );
+};
+
+export default DoubleTabHeartAnimation;
+
+const styles = StyleSheet.create({});
